13033aa62546c5f285a6f9d367eac5e5d8b9508c
alerileri <aleortega@192.168.1.8>
2022-11-08 19:59:27 -0600

    Primer commit con cambios en todos los archivos de src

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,23 @@
 const cipher = {
-  // ...
+  // Código de cifrado y descifrado
+
+  encode(mensaje,desplazamiento){
+    console.log(mensaje);
+    
+    //Variables
+  //Alfabeto (string)
+  let alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  },
+  decode(mensaje,desplazamiento){
+
+  },
 };
 
 export default cipher;
+
+//Funciones
+ 
+  //Fórmula Michelle
+    //(x-65+n)%26+65
+  
+  //Probar switch para insertar el valor de desplazamiento
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+// 1. IMPORTACIÓN DE MÓDULOS
+//Importación del módulo de cifrado y descifrado
 import cipher from './cipher.js';
 
-console.log(cipher);
+// 2. DECLARACIÓN DE VARIABLES
+
+// Variable del texto que se ingrese en la caja de texto
+let mensaje = document.getElementByID("mensaje");
+// Variable del botón descifrar
+let descifrar = document.getElementById("descifrar");
+// Variable del botón cifrar
+let cifrar = document.getElementById("cifrar");
+// variable input de números / desplazamiento
+let numeroClave = document.getElementById("numeroClave");
+
+
+// 3. DECLARACIÓN DE FUNCIONES / EJECUCIÓN DEL CÓDIGO
+//Función para elegir entre cifrar y descifrar; usar el módulo cipher
+//Función para que devuelva el mensaje
+cifrar.addEventListener("click", function () {
+    let valueMessage = mensaje.value;
+    let valueOffset = numeroClave.value;
+    let textEncode = cipher.encode(valueMessage, valueOffset)
+    console.log(textEncode)
+});
