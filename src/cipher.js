173bb45f974fdbd977106cd5268c289425f245b5
alerileri <aleortega@192.168.1.8>
2022-11-10 21:45:45 -0600

    actualización cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,75 @@
 const cipher = {
   // Código de cifrado y descifrado
 
-  encode(mensaje,desplazamiento){
-    console.log(mensaje);
-  
-    //Variables
-  //Alfabeto (string)
-  let alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //Checklist
+  //1. Obtener las letras del mensaje como un string [YA]
+  //2. Obtener el número de posición de cada elemento de ese string [YA]
+  //comparándolo con el string alfabeto
+  //3. Sumarle a esa posición el número de desplazamiento (offset) [YA]
+  //4. Generar un nuevo string con la posición nueva
+  //5. Convertir el arreglo encriptado a una cadena/string
+
+  encode(mensaje, desplazamiento) {
+
+    const encrypt = (char) =>
+      String.fromCharCode((char.charCodeAt() - 65 + 3) % 26 + 65);
+
+    const encryptMessage = (mensaje) =>
+      mensaje.split('').map((el) =>
+        encrypt(el));
+
+    //Enviar error si no ingresa un mensaje a cifrar o descifrar (string)
+    if (mensaje == []) {
+      throw new TypeError("Debes ingresar un número clave");
+
+      console.log(mensaje);
+
+      //Variables
+
+      //Variable para obtener el ASCII del string mensaje y después desplazarlo 
+      // con el offset ingresado en el input
+
+
+      //Regresar el arreglo a una cadena
+
+    }
 
   },
-  decode(mensaje,desplazamiento){
+  decode(mensaje, desplazamiento) {
     console.log(mensaje);
-    let alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
   },
-};
 
-export default cipher;
+  export default cipher;
 
-//Funciones
- 
+
+  //Funciones
+
   //Fórmula Michelle
-    //(x-65+n)%26+65
-  
+  //(x-65+n)%26+65
+  // let 
+
+  //"a".charCodeAt()
+  //"Z".charCodeAt
+  //"A".charCodeAt()
+  //("A".charCodeAt()-65+3)%26+65
+
+  //encode(mensaje,desplazamiento){
+
+  //const encrypt = (char)=>
+  // String.fromCharCode((char.charCodeAt()-65+3)%26+65);
+
+  //const encryptMessage = (mensaje) => 
+  //mensaje.split('').map((el) => 
+  //encrypt (el));
+
+
+
+
+
+
+
+
+}
   //Probar switch para insertar el valor de desplazamiento
